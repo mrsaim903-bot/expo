8a9d377b496a4aa6e7af9503a921d3924a5c2089
Jakub Tkacz <jtkacz@expo.io>
2025-11-13 17:11:08 +0100

    [expo-router] Use UIApplication to get view from nativeTag

--- packages/expo-router/ios/LinkPreview/LinkPreviewNativeZoomTransitionEnabler.swift
+++ packages/expo-router/ios/LinkPreview/LinkPreviewNativeZoomTransitionEnabler.swift
@@ -34,7 +34,7 @@ class LinkPreviewNativeZoomTransitionEnabler: ExpoView {
           image: UIImage(systemName: "arrow.right"), style: .plain, target: self,
           action: nil)
         controller.navigationItem.leftBarButtonItems = [topLeftButton, topLeftButton1]
-          controller.navigationItem.title="Tessst"
+        controller.navigationItem.title = "Tessst"
         return
       }
     } else {
@@ -51,13 +51,15 @@ class LinkPreviewNativeZoomTransitionEnabler: ExpoView {
         // From iOS 18
         if #available(iOS 18.0, *) {
           // print controller
-          print(controller)
+          print("didMoveToSuperview controller", controller)
           // get screenId from controller
           let screenId = LinkPreviewNativeNavigationObjC.getScreenId(controller.view)
           print("⚠️ Found screenId: \(String(describing: screenId))")
+
+          // TODO: Find a better way to get the view from native tag
+          let view: UIView? = UIApplication.shared.keyWindow?.rootViewController?.view.viewWithTag(
+            self.zoomViewNativeTag)
           controller.preferredTransition = .zoom(options: nil) { context in
-            let view: UIView? = controller.navigationController?.view.viewWithTag(
-              self.zoomViewNativeTag)
             print("View from native tag \(self.zoomViewNativeTag): \(String(describing: view))")
             return view
           }
@@ -73,7 +75,7 @@ class LinkPreviewNativeZoomTransitionEnabler: ExpoView {
   private func findViewController() -> UIViewController? {
     var responder: UIResponder? = self
     while let r = responder {
-      print("\(r)")
+      //      print("\(r)")
       if LinkPreviewNativeNavigationObjC.isRNScreen(r) {
         return r as? UIViewController
       }
